f82717a799c5c80a151169915e4655ba3e965330
jiyeong99 <1999wldud@gmail.com>
2022-10-21 13:12:05 +0900

    signup

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,10 @@
+from django import views
 from django.urls import path
+from . import views
+
+app_name = 'accounts'
 
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('signup/', views.signup, name='signup'),
 ]
