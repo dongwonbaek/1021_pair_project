c617bf55cfc8f1422024d4ace66a477146df1801
lsb0709 <dltkdqor0709@gmail.com>
2022-10-21 15:23:57 +0900

    reviews/grade

--- reviews/models.py
+++ reviews/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.conf import settings
 
 # Create your models here.
@@ -7,7 +8,7 @@ class Review(models.Model):
     title = models.CharField(max_length=80)
     content = models.TextField()
     movie_name = models.CharField(max_length=40)
-    grade = models.IntegerField()
+    grade = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)], help_text="0~5사이 값으로 입력하세요")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
