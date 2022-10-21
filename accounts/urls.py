865e249ebcd34ffa8675fb0a06a292f05b62b6be
jiyeong99 <1999wldud@gmail.com>
2022-10-21 13:24:44 +0900

    accounts/login

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,4 +7,5 @@ app_name = 'accounts'
 urlpatterns = [
     path('', views.index, name='index'),
     path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,18 @@
 from multiprocessing import context
 from django.shortcuts import redirect, render
+from django.urls import is_valid_path
 from .forms import CustomUserCreationForm
 from django.contrib.auth import get_user_model
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.forms import AuthenticationForm
 # Create your views here.
 def signup(request):
     if request.method=='POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            auth_login(request.user)
             return redirect('accounts:index')
     else:
         form = CustomUserCreationForm()
@@ -22,3 +27,16 @@ def index(request):
         'users' : users,
     }
     return render(request, 'accounts/index.html', context)
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect(request.GET.get('next') or 'accounts:index')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form' : form,
+    }
+    return render(request, 'accounts/login.html', context)
